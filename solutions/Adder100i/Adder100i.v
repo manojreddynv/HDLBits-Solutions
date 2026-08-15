0c537e553f39ae5ad967696295ee8e89670e55b5
Manoj <manojreddynv2004@gmail.com>
2026-08-16 02:24:02 +0530

    Add HDLBits solution: Adder100i

---
+++ solutions/Adder100i/Adder100i.v
@@ -0,0 +1,25 @@
+﻿module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum 
+);
+    wire [99:0] cin_vector = {cout[98:0], cin};
+
+    fadd fa [99:0] (
+        .a(a),
+        .b(b),
+        .cin(cin_vector),
+        .sum(sum),
+        .cout(cout)
+    );
+
+endmodule
+
+module fadd (
+    input a, b, cin,
+    output sum, cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
